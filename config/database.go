ce579fc7dcbe82991a5effefa4a4fae33b6ac69b
rober0xf <robertinopili13@gmail.com>
2025-04-08 03:10:32 -0300

    database: init db

---
+++ config/database.go
@@ -0,0 +1,27 @@
+package config
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/uptrace/bun"
+	"github.com/uptrace/bun/dialect/pgdialect"
+	"github.com/uptrace/bun/driver/pgdriver"
+	"github.com/uptrace/bun/extra/bundebug"
+)
+
+// bring the database credentials as a input
+func InitDatabase(cfg *Config) *bun.DB {
+	pgconn := pgdriver.NewConnector(
+		pgdriver.WithAddr(fmt.Sprintf("%s:%d", cfg.Database.Host, cfg.Database.Port)),
+		pgdriver.WithUser(cfg.Database.User),
+		pgdriver.WithPassword(cfg.Database.Password),
+		pgdriver.WithDatabase(cfg.Database.Name),
+		pgdriver.WithInsecure(cfg.Database.Insecure),
+	)
+
+	sqldb := sql.OpenDB(pgconn)
+	db := bun.NewDB(sqldb, pgdialect.New())
+	db.AddQueryHook(bundebug.NewQueryHook(bundebug.WithVerbose(true)))
+
+	return db
+}
